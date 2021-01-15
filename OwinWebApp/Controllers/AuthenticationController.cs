f0c471bd5aad8e17990e0f9a846b47867a3cb502
sandro-m <sandrohmorita@gmail.com>
2021-01-14 23:22:25 -0300

    first commit

---
+++ OwinWebApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Owin.Security;
+using OwinWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OwinWebApp.Controllers
+{
+    [AllowAnonymous]
+    public class AuthenticationController : Controller
+    {
+        [HttpGet]
+        // GET: Authentication
+        public ActionResult Login(string ReturnUrl)
+        {
+            var model = new Login
+            {
+                ReturnUrl= ReturnUrl
+            };
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Login model)
+        {
+            if (!ModelState.IsValid) {
+                return View(model);
+            }
+
+            if (model.User == "teste" && model.Senha == "123") {
+                var identity = new ClaimsIdentity(new[] 
+                {
+                    new Claim(ClaimTypes.Name, "Teste Sobrenome"),
+                    new Claim(ClaimTypes.Email, "teste@gmail.com"),
+                    new Claim(ClaimTypes.Country, "Brasil"),
+                    new Claim(ClaimTypes.Role, "Admin")
+                },
+                "ApplicationCookie");
+
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+
+                authManager.SignIn(identity);
+
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+
+            // Falha na autenticação
+            return View();
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            return string.IsNullOrEmpty(returnUrl) || string.IsNullOrWhiteSpace(returnUrl) ? Url.Action("Index", "Home") : returnUrl;
+        }
+
+        public ActionResult Logout()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+            authManager.SignOut("ApplicationCookie");
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ OwinWebApp/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(OwinWebApp.Startup))]
+
+namespace OwinWebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCookieAuthentication(new CookieAuthenticationOptions { 
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Authentication/Login"),
+                ExpireTimeSpan = TimeSpan.FromMinutes(60)
+            });
+        }
+    }
+}
